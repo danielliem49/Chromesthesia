cd78a738a8872a5d435becc7fbf57d71a250bdda
Daniel Liem <danielliem49@gmail.com>
2023-02-07 09:01:48 -0800

    Add UI Functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import Game from "./scripts/game"
-import Input from "./scripts/input"
-
 
 // Setup Canvas
 const canvasWidth = window.innerWidth;
@@ -10,9 +8,66 @@ const canvas = document.getElementById("game-canvas");
 canvas.setAttribute("width", canvasWidth);
 canvas.setAttribute("height", canvasHeight);
 const ctx = canvas.getContext("2d");
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+
+// toggle menu with button
+const toggleBtn = document.getElementById("toggleBtn");
+const controlMenu = document.getElementById("controlMenu");
+toggleBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    controlMenu.style.display = controlMenu.style.display === "none" ? "flex" : "none";
+})
+
+// // toggle menu with space
+// toggleBtn.addEventListener("keydown", (event) => {
+//     if (event.code === "Space") {
+//     event.preventDefault();
+//     event.stopPropagation();
+//     controlMenu.style.display = controlMenu.style.display === "none" ? "flex" : "none";
+//     }
+// })
 
 
 
-// start game
+// initialize game and click listener
 const game = new Game(canvas);
 game.start();
+// canvas.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     event.stopPropagation();
+//     game.running = !game.running;
+//     game.start();
+// })
+
+
+// sliders
+let slider1 = document.getElementById("colorVariation");
+let slider2 = document.getElementById("particleStep");
+let slider3 = document.getElementById("particleBase");
+let slider4 = document.getElementById("xBias");
+let slider5 = document.getElementById("yBias");
+
+game.hueVolatility = slider1.value;
+game.step = slider2.value;
+game.base = slider3.value;
+game.xBias = slider4.value;
+game.yBias = slider5.value;
+
+slider1.addEventListener("input", function () {
+    game.hueVolatility = this.value;
+});
+slider2.addEventListener("input", function () {
+    game.step = this.value;
+});
+slider3.addEventListener("input", function () {
+    game.base = this.value;
+});
+slider4.addEventListener("input", function () {
+    game.xBias = this.value;
+});
+slider5.addEventListener("input", function () {
+    game.yBias = this.value;
+});
--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,5 +1,4 @@
 
-import Input from "./input.js"
 import Particle from "./particle.js"
 import { createNoise4D } from 'simplex-noise';
 
@@ -10,15 +9,16 @@ export default class Game {
         backgroundColor: 'black',
         particlesNum: 1000,
         step: 10,
-        base: 400,
+        base: 300,
+
         xBias: 1, 
-        // 0..2
         yBias: 1, 
-        // 0..2
         zIncrement: 0.001,
         wIncrement: 0.001,
-        alphaIncrement: 0.001,
 
+        hueIncrement: 0.2,
+        hueVolatility: 2,
+        alphaIncrement: 0.001
     };
 
     constructor(canvas) {
@@ -30,37 +30,41 @@ export default class Game {
         this.noise4D = createNoise4D(Math.random);
         this.rand = Math.random();
         
-        this.hueStart = Math.random()* 360;
-
+        // algorithm variable start values
         this.zStart = 0;
         this.wStart = 0;
-        this.gui = 0;
-        
+        this.hueStart = Math.random()* 360;
 
-        this.input = new Input(canvas);
+        // UI adjustable variables
+        this.hueVolatility = Game.DEFAULTS.hueIncrement;
+        this.step = Game.DEFAULTS.step;
+        this.base = Game.DEFAULTS.base;
+        this.xBias = Game.DEFAULTS.xBias;
+        this.yBias = Game.DEFAULTS.yBias;
 
-        this.running = true;
-        this.commanded = false;
-
+        // game state
+        this.running = false;
     }
     
-    addParticles(){
-        for(let i = 0; i < Game.DEFAULTS.particlesNum; i++) {
-            let x = this.DIM_X * Math.random();
-            let y = this.DIM_Y * Math.random();
-            // color hue in degrees
-            let color = { h: this.hueStart + Math.atan2((this.DIM_Y / 2) - y, this.DIM_X - x) * 180 / Math.PI, s: 1, l : 0.5, a : 0
-        }
-
-            this.particles.push(new Particle(x, y, color));
-        }
-    }
-
     resetParticle(particle){
         particle.x = this.DIM_X * Math.random();
         particle.y = this.DIM_Y * Math.random();
+        // color hue in degrees
         particle.color = {
-            h: this.hueStart + Math.atan2((this.DIM_Y / 2) - particle.y, this.DIM_X - particle.x) * 180 / Math.PI, s: 1, l: 0.5, a: 0}
+            h: (this.hueStart + (this.hueVolatility * Math.atan2(this.DIM_Y - particle.y, this.DIM_X - particle.x) * 180 / Math.PI)), s: 1, l: 0.5, a: 0}
+        }
+
+    addParticles() {
+
+        if (this.particles.length) {
+            this.particles = [];
+        }
+
+        for (let i = 0; i < Game.DEFAULTS.particlesNum; i++) {
+            let particle = new Particle(0, 0, {});
+            this.resetParticle(particle);
+            this.particles.push(particle);
+        }
     }
 
     getNoise(x, y, z, w) {
@@ -68,12 +72,12 @@ export default class Game {
 
         for (let i = 0; i < octaves; ++i) {
             amp *= fallout;
-            sum += amp * (this.noise4D(x * f, y * f, z * f, w * f) + 1);
+            sum += amp * (this.noise4D(x * f, y * f, z * f, w * f));
+            // console.log(this.noise4D(x * f, y * f, z * f, w * f));
             f *= 2;
         }
-
         return sum;
-    } 
+    }
 
     // click(event) {
     //     this.ctx.save();
@@ -85,28 +89,30 @@ export default class Game {
     //     this.noise4D = createNoise4D(Math.random);
     // }
 
-    step() {
-        let step = Game.DEFAULTS.step, base = Game.DEFAULTS.base, t = 5000;
-
+    update() {
+        let step = this.step, base = this.base, t = 5000;
+        
         for (let i = 0; i < this.particles.length; i++) {
             let p = this.particles[i];
-
+            
             
             // updates new pos of x and y, and saves old pos
             p.lastX = p.x;
             p.lastY = p.y;
-
-            let angle = Math.PI * 6 * this.getNoise((p.x / base) * Game.DEFAULTS.xBias, (p.y / base) * Game.DEFAULTS.yBias, this.zStart, this.wStart);
+            
+            let angle = Math.PI * 6 * this.getNoise((p.x / base) * this.xBias, (p.y / base) * this.yBias, this.zStart, this.wStart);
             p.x += Math.cos(angle) * step;
             p.y += Math.sin(angle) * step;
+            // console.log(angle)
 
 
             // makes more opaque over time
             if (p.color.a < 1) p.color.a += Game.DEFAULTS.alphaIncrement;
-
-
+            
+            
             // actual drawing
             this.ctx.beginPath();
+
             this.ctx.strokeStyle = p.hsla();
             this.ctx.moveTo(p.lastX, p.lastY);
             this.ctx.lineTo(p.x, p.y);
@@ -126,33 +132,33 @@ export default class Game {
             // this.ctx.stroke();
             // }, t)
 
-
         }
 
-
         if (this.rand > 0.5) {
-            this.hueStart += 0.1;
+            this.hueStart += Game.DEFAULTS.hueIncrement;
         }else{
-            this.hueStart -= 0.1;
+            this.hueStart -= Game.DEFAULTS.hueIncrement;
         }
 
         this.zStart += Game.DEFAULTS.zIncrement;
         this.wStart += Game.DEFAULTS.wIncrement;
 
-        requestAnimationFrame(this.step.bind(this));
+        requestAnimationFrame(this.update.bind(this));
     }
 
-    
     start() {
-        this.ctx.clearRect(0, 0, this.DIM_X, this.DIM_Y);
-        this.ctx.fillStyle = Game.DEFAULTS.backgroundColor;
-        this.ctx.fillRect(0, 0, this.DIM_X, this.DIM_Y);
-        this.addParticles();
-        requestAnimationFrame(this.step.bind(this));
-        // setInterval(() => this.draw(), 1000/60);
-        // setInterval(() => {this.input.showCursorPos()}, 1000 / 60);
-        // setInterval(() => {this.input.showRcPos()}, 1000 / 60);
-        console.log("Game is Running!");
+        // if (this.running){
+            this.addParticles();
+            requestAnimationFrame(this.update.bind(this));
+            console.log("Game is Running!");       
+
+        // }else{
+        //     cancelAnimationFrame(this.raf);
+        //     this.ctx.clearRect(0, 0, this.DIM_X, this.DIM_Y);
+        //     this.ctx.fillStyle = Game.DEFAULTS.backgroundColor;
+        //     this.ctx.fillRect(0, 0, this.DIM_X, this.DIM_Y);
+        // }
+        console.log(this.running)
     }
 
 }
